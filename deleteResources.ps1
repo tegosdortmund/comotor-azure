03b7c007ba4a5184fc9a7bce6fd7dbc30c93d232
Sören Straßmann <sstrassmann@tegos.eu>
2015-11-30 10:09:01 +0100

    k

--- deleteResources.ps1
+++ deleteResources.ps1
@@ -1,6 +1,6 @@
 ﻿
 
-New-AzureRmResourceGroupDeployment -Name comotor -ResourceGroupName tegosDTM-SStrassmann -TemplateUri 'https://raw.githubusercontent.com/tegosdortmund/comotor-azure/master/azuredeploy.json' -TemplateParameterFile 'C:\Users\sstrassmann\Desktop\Seminararbeit\other resources\azuredeploy.param.dev.json'
+New-AzureRmResourceGroupDeployment -Name comotor42 -ResourceGroupName comotor -TemplateUri 'https://raw.githubusercontent.com/tegosdortmund/comotor-azure/master/azuredeploy.json' -TemplateParameterFile 'C:\Users\sstrassmann\Desktop\Seminararbeit\other resources\azuredeploy.param.dev.json'
 
 
 $secpasswd = ConvertTo-SecureString '' -AsPlainText -Force
@@ -11,7 +11,7 @@ Login-AzureRmAccount -Verbose -Credential $cred
 
 
 do {
-  $resources = Get-AzureRmResource -ResourceGroupName 'tegosDTM-SStrassmann' |  where { (!$_.Name.Contains('notdelete')) -and (!$_.Name.Contains('tegosDTM-SStrassmann')) -and (!$_.Name.Contains('asdafssad')) }
+  $resources = Get-AzureRmResource -ResourceGroupName 'comotor' |  where { (!$_.Name.Contains('notdelete')) -and (!$_.Name.Contains('tegosDTM-SStrassmann')) -and (!$_.Name.Contains('grau')) }
 
   foreach ($resource in $resources) {
     echo $resource.Name
